6942412e46e7731a03b772014a9475ae0d374b65
Gerardo Alvarez <70041405+Gerard9199@users.noreply.github.com>
2023-03-05 21:41:25 -0600

    new version

--- 02_ingestar.py
+++ 02_ingestar.py
@@ -1,31 +1,28 @@
-#carga
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def split_data(dataframe_name, validation=False):
-    df = pd.read_csv(dataframe_name+".csv")
+def data_structure(train_1, train_2, df_name)
+    _df = train_1.copy()
+    _df['test_result'] = train_2
+    _df.to_csv(df_name+'.csv', encoding='utf-8')
+
+def split_data(dataframe_name, validation=False, ss=0.1):
+    datasets = pd.DataFrame()
+    df = pd.read_csv(dataframe_name)
     X = df.drop('test_result',axis=1) 
     y = df['test_result']
     
-    if validation == False: 
-      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=101)
-      dataset_train = pd.DataFrame()
-      dataset_train['age']= X_train['age']
-      dataset_train['physical_score']= X_train['physical_score']
-      dataset_train['test_result']= y_train
-      dataset_train.to_csv('train.csv',index=False)
-      print("Dataset train.csv listo ...")
-      dataset_test = pd.DataFrame()
-      dataset_test['age']= X_test['age']
-      dataset_test['physical_score']= X_test['physical_score']
-      dataset_test['test_result']= y_test
-      dataset_test.to_csv('test.csv',index=False)
-      print("Dataset test.csv listo ...")
-    else :
-        #para que lo complete uno de ustedes
-       pass 
-    return
+    (X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=ss, random_state=101)
     
+    if validation == False:
+        setname_01 = 'train'
+        setname_02 = 'test'
+    else:
+        setname_01 = 'train_01'
+        setname_02 = 'train_02'
+        
+    data_structure(X_train, y_train, setname_01)
+    data_structure(X_test, y_test, setname_02)
 
-
-split_data('hearing_test', validation = False)
+split_data('hearing_test.csv', validation=False, ss=0.1)
+split_data('train.csv', validation=True, ss=0.3)
